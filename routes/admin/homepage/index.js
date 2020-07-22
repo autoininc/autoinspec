5a9a724ec3ce0caf70e6bcb23fff2aee04f8404b
Ubuntu <ubuntu@ip-172-26-9-16.ap-northeast-2.compute.internal>
2020-07-22 07:54:17 +0000

    CRM server

--- routes/admin/homepage/index.js
+++ routes/admin/homepage/index.js
@@ -7,8 +7,8 @@ const controller = require('./homepage.controller');
 const multer = require("multer");
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, '/home/hosting_users/autoinspec/apps/autoinspec_autoinspec/public/uploads/')
-        // cb(null, './upload/homepage/')
+        //cb(null, '/home/hosting_users/autoinspec/apps/autoinspec_autoinspec/public/uploads/')
+        cb(null, 'public/uploads');
     },
     filename: function(req, file, cb) {
         var mimeType;
